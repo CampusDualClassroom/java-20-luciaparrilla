1eb23083aa9a331ad1fa356e3d93bdfa873ba635
Lucía Parrilla del Río <luciaparrilla9@hotmail.com>
2024-10-22 16:59:56 +0200

    Terminado 20

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,18 +6,26 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
-
+        List<Person> people = new ArrayList<>();
+        people.add(new Person ("John", "Smith"));
+        people.add(new Teacher("María", "Montessori", "Educación"));
+        people.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        people.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return people;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        if (stringList == null || stringList.isEmpty()) {
+            System.out.println("There's no one to show.");
+        } else {
+            for (Person person : stringList) {
+                person.getDetails();
+            }
+        }
     }
 
     public static void main(String[] args) {
         List<Person> stringList = getPeopleList();
         showPeopleDetails(stringList);
     }
-
-
-
 }
--- src/main/java/com/campusdual/classroom/Teacher.java
+++ src/main/java/com/campusdual/classroom/Teacher.java
@@ -8,4 +8,10 @@ public class Teacher extends Person {
 		super(name, surname);
 		this.area = area;
 	}
+
+	@Override
+	public void getDetails() {
+		super.getDetails();
+		System.out.println("Area: " + area);
+	}
 }
